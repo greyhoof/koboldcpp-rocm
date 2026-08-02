da8a5e44613a2e3027004f04516e621e5505cad6
Concedo <39025047+LostRuins@users.noreply.github.com>
2026-08-02 10:33:54 +0800

    bump version

--- koboldcpp.py
+++ koboldcpp.py
@@ -79,7 +79,7 @@ dry_seq_break_max = 128
 extra_images_max = 4 # for kontext/qwen img
 
 # global vars
-KcppVersion = "1.118"
+KcppVersion = "1.118.1"
 showdebug = True
 kcpp_instance = None #global running instance
 global_memory = {"tunnel_url": "", "restart_target":"", "input_to_exit":False, "load_complete":False, "restart_override_base_config":"", "last_active_timestamp":datetime.now(), "triggered_sleeping":False, "current_model":"initial_model", "base_config":"", "swapReqType": None, "autoswapmode": False}
